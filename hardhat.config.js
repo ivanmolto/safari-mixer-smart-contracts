1aaa2f16d63c98579d2abdaf5e0c858930752165
Ivan Molto <ivanmolto@gmail.com>
2022-06-24 21:41:43 +0200

    chore: Add Hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy")
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -10,12 +12,40 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+        {
+            version: "0.8.8",
+        },
+        {
+            version: "0.6.6",
+        },
+    ],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    polygonMumbai: {
+      chainId: 80001,
+      url: process.env.ALCHEMY_API_KEY_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      gas: 2100000, 
+      gasPrice: 54625545793,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY
+    }
+  },
+  namedAccounts: {
+    deployer: {
+        default: 0, // here this will by default take the first account as deployer
+        1: 0,
+        80001: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  } ,
 };
